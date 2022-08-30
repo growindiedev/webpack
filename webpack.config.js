d2052160dcafbe3e8cbe984b3ca8815fc93ffcd6
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-08-30 10:13:35 +0530

    add yaml,toml and json5 files @

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "production",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -11,6 +12,18 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
       },
     ],
   },
